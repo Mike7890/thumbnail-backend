c1d2f5ca03ab7e220813040c9850efff622e435f
Mike <48123901+Mike7890@users.noreply.github.com>
2021-05-03 16:06:14 +0530

    changes

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ app.post("/upload", uploadImage, (req, res, next) => {
         "BACKENDURL"
       )}/uploads/thumbnails-${req.file.originalname.replace(/\s/g, "")}`,
     });
-    });
-  } catch (error) {
+
+  c} catch (error) {
     res.status(500).send("Internal Server Error");
   }
 });
